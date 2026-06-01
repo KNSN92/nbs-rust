a837cc95f293b88b537d9689eab9ab1c454e1d8d
KNSN92 <kekenshin92@gmail.com>
2026-06-01 10:58:25 +0900

    feat: HZ_44100とHZ_48000を追加

--- src/audio/mod.rs
+++ src/audio/mod.rs
@@ -16,3 +16,6 @@ pub type Sample = Float;
 pub type Frame = [Sample; 2]; // Nbs sound is stereo, so 2 channels
 pub type SampleRate = NonZeroU32;
 pub type Channels = NonZeroU16;
+
+pub const HZ_44100: SampleRate = unsafe { NonZeroU32::new_unchecked(44100) };
+pub const HZ_48000: SampleRate = unsafe { NonZeroU32::new_unchecked(48000) };
