210f5b412f2206ddaccd905a15cd5a934c7cc0af
KNSN92 <kekenshin92@gmail.com>
2026-09-03 17:17:33 +0900

    update: from_vecで与えられたvecの最後に空のフレームがあればそれを最後のパディングとして利用するように

--- src/audio/note_audio.rs
+++ src/audio/note_audio.rs
@@ -40,9 +40,13 @@ pub struct Frames(usize, Arc<[Frame]>);
 
 impl Frames {
     pub fn from_vec(mut frames: Vec<Frame>) -> Self {
-        let len = frames.len();
-        //* 8フレーム分のパディングを追加する。これにより、SIMDでの読み取り時に境界チェックを行わずに済む。
-        frames.extend(repeat_n([0.0, 0.0], 8));
+        let mut len = frames.len();
+        //* 8フレーム分のパディングを追加する。ただし、最後の8フレームがすでに0.0で埋まっている場合はそれを利用し、lenを8減らす。これにより、SIMDでの読み取り時にバッファオーバーフローチェックが不要になる。
+        const EMPTY_CHUNK: [Frame; 8] = [[0.0, 0.0]; 8];
+        match frames.last_chunk::<8>() {
+            Some(&EMPTY_CHUNK) => len -= 8,
+            _ => frames.extend(repeat_n([0.0, 0.0], 8)),
+        }
         Frames(len, frames.into())
     }
 }
